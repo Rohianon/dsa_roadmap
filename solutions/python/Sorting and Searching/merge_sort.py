107c243b0ffba2d41c219b95cd7951edc590843f
Rohianon <rohianon48@gmail.com>
2025-01-04 21:41:32 +0300

    cleanup, improve structure, additional test case

--- solutions/python/Sorting and Searching/merge_sort.py
+++ solutions/python/Sorting and Searching/merge_sort.py
@@ -3,13 +3,8 @@ from merge import merge
 
 def merge_sort(arr: List[int]) -> Optional[callable]:
     if len(arr) == 1: return arr
-    # print(arr)
 
     mid_point = len(arr)//2
-    # print(mid_point)
-
-    # print('h1', arr[:mid_point])
-    # print('h2', arr[mid_point:])
 
     left_half = merge_sort(arr[:mid_point])
     right_half = merge_sort(arr[mid_point:])
@@ -21,5 +16,16 @@ def merge_sort(arr: List[int]) -> Optional[callable]:
 if __name__ == "__main__":
     arr = [8, 40, 6, 4, 11, 5, 7]
     actual = [4, 5, 6, 7, 8, 11, 40]
-    print(merge_sort(arr))
-    # assert(arr == actual)
+    sorted_arr = merge_sort(arr)
+    print(sorted_arr)
+    assert sorted_arr == actual, 'not sorted!'
+
+    arr = [6]
+    actual = [6]
+    sorted_arr = merge_sort(arr)
+    print(sorted_arr)
+    assert sorted_arr == actual, 'not sorted!'
+
+
+# TC: O(n log n)
+# SP: O(n) - if implemented well, we could end up with O(1)
